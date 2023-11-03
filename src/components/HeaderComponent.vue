c0253d0b644312d0753b6350583843d0dd83eb25
AndersonShen <shen.andersonshen@gmail.com>
2023-11-03 16:40:20 +0800

    feat: 增加 header 的上方結構

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,4 +1,35 @@
 <script setup></script>
 <template>
-  <div>Header</div>
+  <header>
+    <div class="container-fluid">
+      <div class="px-6 py-4">
+        <nav class="d-flex justify-content-between align-items-center px-6 py-3">
+          <h4 class="mb-0">
+            <RouterLink to="/">
+              <img src="../assets/images/logo.svg" alt="logo" />
+              <span class="ms-2">喵立翰 Miao Li-Han</span>
+            </RouterLink>
+          </h4>
+          <ul class="list-unstyled d-lg-flex d-none gap-4 mb-0">
+            <li><a href="#1">候選人主張</a></li>
+            <li><a href="#2">最新活動</a></li>
+            <li><a href="#3">政策議題</a></li>
+            <li><a href="#4">小額捐款</a></li>
+            <li><a href="#5">民眾服務信箱</a></li>
+          </ul>
+          <ul class="list-unstyled d-md-flex d-none gap-4 mb-0">
+            <li>
+              <a href="#"><img src="../assets/images/icons/icon-fb.svg" alt="fb" /></a>
+            </li>
+            <li>
+              <a href="#"><img src="../assets/images/icons/icon-ig.svg" alt="ig" /></a>
+            </li>
+            <li>
+              <a href="#"><img src="../assets/images/icons/icon-youtu.svg" alt="yt" /></a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </header>
 </template>
--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -1,4 +1,11 @@
 <script setup></script>
+
 <template>
-  <div>Index</div>
+  <div class="container">
+    <div class="vh-100 bg-primary" id="1"></div>
+    <div class="vh-100 bg-secondary" id="2"></div>
+    <div class="vh-100 bg-success" id="3"></div>
+    <div class="vh-100 bg-danger" id="4"></div>
+    <div class="vh-100 bg-warning" id="5"></div>
+  </div>
 </template>

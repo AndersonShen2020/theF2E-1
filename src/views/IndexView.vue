f6f3dde716568a86cecfb14a96825a2533c4b9c3
AndersonShen <shen.andersonshen@gmail.com>
2023-11-08 17:53:37 +0800

    feat: 增加 Banner 區塊

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -1,11 +1,41 @@
 <script setup></script>
 
 <template>
+  <!-- 台灣的明天 喵先鋪路 -->
   <div class="container">
-    <div class="vh-100 bg-primary" id="1"></div>
-    <div class="vh-100 bg-secondary" id="2"></div>
-    <div class="vh-100 bg-success" id="3"></div>
-    <div class="vh-100 bg-danger" id="4"></div>
-    <div class="vh-100 bg-warning" id="5"></div>
+    <div class="banner" id="1">
+      <div class="container">
+        <div class="d-flex flex-column justify-content-center">
+          <h3 class="bg-primary-gradient text-center font-mantou text-clip pt-lg-24 fz-96 lh-105">
+            台灣的明天 喵先鋪路
+          </h3>
+          <div class="d-flex justify-content-center align-items-center mt-4">
+            <h3 class="lh-1 py-3 px-4 me-4 mb-0 border rounded-4 text-white fw-bold bg-slate-700">2024 立委參選人</h3>
+            <h1 class="lh-base mb-0 d-flex align-items-center logo-primary py-3 px-4">
+              <span
+                class="align-items-center bg-primary-gradient d-inline-flex justify-content-center rounded-circle text-white logo-number fs-2 me-4"
+              >
+                3
+              </span>
+              <p class="mb-0 text-primary fw-bold">喵立翰 Miao Li-Han</p>
+            </h1>
+          </div>
+          <div class="mt-2">
+            <div class="d-flex justify-content-center">
+              <img class="w-75" src="../assets/images/pictures/image 12.webp" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
+  <!-- 為喵星人，護台灣！ 從喵的眼中，看見台灣 喵的未來，人的希望 -->
+  <!-- 候選人主張 -->
+  <div class="vh-100 bg-secondary" id="2"></div>
+  <!-- 最新活動 -->
+  <div class="vh-100 bg-success" id="3"></div>
+  <!-- 政策議題 -->
+  <div class="vh-100 bg-danger" id="4"></div>
+  <!-- 小額支持喵喵 & 民眾服務信箱 -->
+  <div class="vh-100 bg-warning" id="5"></div>
 </template>

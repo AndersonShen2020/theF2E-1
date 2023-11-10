0018e3ad3b9ca3ace384c58ae3032d9687dce36b
AndersonShen <shen.andersonshen@gmail.com>
2023-11-10 16:27:48 +0800

    fix: 修正跑馬燈的 overflow-hidden 錯誤

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -33,7 +33,7 @@
     </div>
   </div>
   <!-- 為喵星人，護台灣！ 從喵的眼中，看見台灣 喵的未來，人的希望 -->
-  <div class="bg-primary overflow-hiddene">
+  <div class="bg-primary overflow-hidden">
     <ul class="list-unstyled lh-105 text-white font-mantou marquee d-flex gap-6 text-nowrap mb-0 position-relative">
       <li>為喵星人，護台灣！</li>
       <li>從喵的眼中，看見台灣</li>
@@ -41,7 +41,25 @@
     </ul>
   </div>
   <!-- 候選人主張 -->
-  <div class="vh-100 bg-secondary" id="2"></div>
+  <div class="bg-theme-2" id="2">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 col-12">
+          <p>ADVOCATE</p>
+          <h2>候選人主張</h2>
+          <h3>我堅信 ! 藉由推動更完善的貓咪福利和相關政策，更是間接地投資於台灣的未來。</h3>
+          <p>
+            畢竟，民眾的身心健康與工作熱情是推動經濟的核心動力。透過完善的貓咪福利政策，為台灣的 GDP 經濟帶來巨大效益。
+            <br />
+            因此，我期望能在立法院內推進這些政策，確保每一隻貓咪都能得到他們應有的照顧，同時也為台灣的經濟發展助一臂之力。讓我們一同護航台灣的幸福經濟，從照顧每一隻貓咪開始。
+          </p>
+        </div>
+        <div class="col-lg-6 col-12">
+          <img class="img-fluid" src="../assets/images/pictures/image 8.webp" alt="advocate" />
+        </div>
+      </div>
+    </div>
+  </div>
   <!-- 最新活動 -->
   <div class="vh-100 bg-success" id="3"></div>
   <!-- 政策議題 -->

9ece86453fb51d56388fcf6b644a48f9e4d172c2
AndersonShen <shen.andersonshen@gmail.com>
2023-11-20 21:24:00 +0800

    feat: 增加 Footer 的 RWD

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -2,42 +2,44 @@
 <template>
   <footer>
     <div class="py-16 bg-theme-2">
-      <div class="d-flex justify-content-center gap-4">
-        <div class="d-flex flex-column footer-content-box">
-          <h2 class="lh-base mb-0 d-flex align-items-center logo-primary border-0 shadow-none bg-theme-2">
-            <span
-              class="align-items-center bg-primary-gradient d-inline-flex justify-content-center rounded-circle text-white footer-number-logo fs-3 me-4"
-            >
-              3
-            </span>
-            <p class="mb-0 text-primary fw-bold fs-3">喵立翰 Miao Li-Han</p>
-          </h2>
-          <ul class="list-unstyled d-md-flex d-none gap-2 mt-4 mb-0">
-            <li>
-              <a href="#"><img class="footer-icon" src="../assets/images/icons/icon-fb.svg" alt="fb" /></a>
-            </li>
-            <li>
-              <a href="#"><img class="footer-icon" src="../assets/images/icons/icon-ig.svg" alt="ig" /></a>
-            </li>
-            <li>
-              <a href="#"><img class="footer-icon" src="../assets/images/icons/icon-youtu.svg" alt="yt" /></a>
-            </li>
-          </ul>
-          <p class="mb-0 mt-4 fz-copyright">© 2023 喵立翰 Miao Li-Han 版權所有</p>
-        </div>
-        <div class="d-flex flex-column footer-content-box">
-          <h2 class="mb-0 fs-4 fw-bold">競選總部</h2>
-          <ul class="list-unstyled mt-4 mb-0">
-            <li>
-              辦公室地址：<a
-                href="https://www.google.com/maps/search/%E5%96%B5%E6%98%9F%E5%8D%80%EF%BC%8C%E6%AF%9B%E8%8C%B8%E8%8C%B8%E5%A4%A7%E9%81%9388%E8%99%9F%EF%BC%8C%E5%96%B5%E5%96%B5%E5%A4%A7%E6%A8%933%E6%A8%93/"
-                target="_blank"
-                >喵星區，毛茸茸大道88號，喵喵大樓3樓</a
+      <div class="container-md">
+        <div class="d-flex flex-lg-row flex-column justify-content-center gap-lg-4 gap-6">
+          <div class="d-flex flex-column footer-content-box">
+            <h2 class="lh-base mb-0 d-flex align-items-center logo-primary border-0 shadow-none bg-theme-2">
+              <span
+                class="align-items-center bg-primary-gradient d-inline-flex justify-content-center rounded-circle text-white footer-number-logo fs-3 me-4"
               >
-            </li>
-            <li>電話：<a href="tel:(02)888-5678">(02) 888-5678 </a></li>
-            <li>電子郵件地址：<a href="mailto:meowoffice@linmeow.tw">meowoffice@linmeow.tw</a></li>
-          </ul>
+                3
+              </span>
+              <p class="mb-0 text-primary fw-bold fs-3">喵立翰 Miao Li-Han</p>
+            </h2>
+            <ul class="list-unstyled d-flex gap-2 mt-4 mb-0">
+              <li>
+                <a href="#"><img class="footer-icon" src="../assets/images/icons/icon-fb.svg" alt="fb" /></a>
+              </li>
+              <li>
+                <a href="#"><img class="footer-icon" src="../assets/images/icons/icon-ig.svg" alt="ig" /></a>
+              </li>
+              <li>
+                <a href="#"><img class="footer-icon" src="../assets/images/icons/icon-youtu.svg" alt="yt" /></a>
+              </li>
+            </ul>
+            <p class="mb-0 mt-4 fz-copyright">© 2023 喵立翰 Miao Li-Han 版權所有</p>
+          </div>
+          <div class="d-flex flex-column footer-content-box">
+            <h2 class="mb-0 fs-4 fw-bold">競選總部</h2>
+            <ul class="list-unstyled mt-4 mb-0">
+              <li>
+                辦公室地址：<a
+                  href="https://www.google.com/maps/search/%E5%96%B5%E6%98%9F%E5%8D%80%EF%BC%8C%E6%AF%9B%E8%8C%B8%E8%8C%B8%E5%A4%A7%E9%81%9388%E8%99%9F%EF%BC%8C%E5%96%B5%E5%96%B5%E5%A4%A7%E6%A8%933%E6%A8%93/"
+                  target="_blank"
+                  >喵星區，毛茸茸大道88號，喵喵大樓3樓</a
+                >
+              </li>
+              <li>電話：<a href="tel:(02)888-5678">(02) 888-5678 </a></li>
+              <li>電子郵件地址：<a href="mailto:meowoffice@linmeow.tw">meowoffice@linmeow.tw</a></li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>

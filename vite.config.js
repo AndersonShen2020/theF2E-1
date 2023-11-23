488025934013ee908abf065c77a9c8cd2a0beeb7
AndersonShen <shen.andersonshen@gmail.com>
2023-11-23 21:39:15 +0800

    feat: 清除自定義元件 "swiper-" 的警告

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,15 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/theF2E-1/",
-  plugins: [vue()],
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) => tag.startsWith("swiper-")
+        }
+      }
+    })
+  ],
   css: {
     devSourcemap: true
   },

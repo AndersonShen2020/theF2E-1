a1ac09465b3dcc12ddb770cbce2329d6e1b5c292
AndersonShen <shen.andersonshen@gmail.com>
2023-11-23 18:31:31 +0800

    3rd: 更新 ESLint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,5 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution")
+require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
@@ -8,6 +8,7 @@ module.exports = {
     "plugin:vue/vue3-essential",
     "eslint:recommended",
     "@vue/eslint-config-prettier/skip-formatting",
+    "standard",
     "prettier"
   ],
   parserOptions: {
@@ -21,4 +22,4 @@ module.exports = {
       }
     ]
   }
-}
+};
